4eb2ccbc62b5589c9af4ba6733b9cf0a48fd1713
Douglas <douglas.nascimentosud21@gmail.com>
2026-05-26 00:25:18 -0300

    .

--- js/script.js
+++ js/script.js
@@ -15,3 +15,34 @@ links.forEach((link) => {
     menuHamburguer.classList.remove("active");
   });
 });
+
+/* Cursor */
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', (e) => {
+
+  cursor.style.left = e.clientX + 'px';
+  cursor.style.top = e.clientY + 'px';
+
+});
+
+const hoverItems = document.querySelectorAll('a, button')
+
+hoverItems.forEach(item => {
+
+  item.addEventListener('mouseenter', () => {
+
+  cursor.style.width = '40px';
+  cursor.style.heigth = '40px';
+
+  })
+  
+  item.addEventListener('mouseleave', ()=> {
+
+  cursor.style.width = '22px';
+  cursor.style.heigth = '22px';
+
+});
+
+});
